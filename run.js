41da66367f0f71e1f5ab1ef30e9450c0c0a32828
Erkhembayr Batjargal <236621357+Ericwasepic127@users.noreply.github.com>
2026-09-15 10:42:11 +0800

    Update run.js

--- run.js
+++ run.js
@@ -2,10 +2,12 @@
 // Usage:
 // <p id="statusBox">JavaScript is disabled in this browser. Please enable or ensure your browser supports JavaScript</p>
 // <script src="https://ericwasepic127.github.io/run.js"></script>
-
-const status = document.getElememtById("statusBox");
-if (typeof WebAssembly === "object" && typeof WebAssembly.instantiate === "function") {
+const loads = function() {
+  const status = document.getElementById("statusBox");
+  if (typeof WebAssembly === "object" && typeof WebAssembly.instantiate === "function") {
     status.remove();
-} else {
+  } else {
     status.innerHTML = "WebAssembly is disabled in this browser. Please enable or ensure your browser supports WebAssembly";
+  }
 }
+document.addEventListener("DOMContentLoaded", loads)
